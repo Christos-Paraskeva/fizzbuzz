1c1491773f0b3ab64ce4d493175042d18278f6c5
Christos <chris.paraskeva10@gmail.com>
2017-02-09 16:16:17 +0000

    completed fizzbuzz challenges

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
 def fizzbuzz(number)
-  'fizz'
+  if (number % 3 == 0) && (number % 5 == 0)
+    'Fizzbuzz'
+  elsif (number % 3 == 0)
+    'Fizz'
+  elsif (number % 5 == 0)
+    'Buzz'
+  else
+    number
+  end
 end
+
+fizzbuzz(20)
